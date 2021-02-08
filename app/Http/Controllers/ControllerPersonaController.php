a400cffe2fa78d260463671b6f3dc7181ecab377
djcorea <javier.corea27@gmail.com>
2021-02-08 02:42:11 -0600

    First Commit

---
+++ app/Http/Controllers/ControllerPersonaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ControllerPersona;
+use Illuminate\Http\Request;
+
+class ControllerPersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ControllerPersona  $controllerPersona
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ControllerPersona $controllerPersona)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ControllerPersona  $controllerPersona
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ControllerPersona $controllerPersona)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ControllerPersona  $controllerPersona
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ControllerPersona $controllerPersona)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ControllerPersona  $controllerPersona
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ControllerPersona $controllerPersona)
+    {
+        //
+    }
+}
